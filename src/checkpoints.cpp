f2b3d0042de3d453ac40afdb92a4d96b58145101
pabloval342 <pablovalla342@gmail.com>
2022-01-06 19:16:11 +0100

    Add ckeckpoint 6.jan 2022
    CP at 644980

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -47,6 +47,7 @@ namespace Checkpoints
         ( 543395,   uint256("0x2ef14111409247ab85c8c3ace85333eefcb0590fadeb28a7e79217a4df43e749") )
         ( 560377,   uint256("0x484e5af078c27931f29ddc6a905619fffce13c65ef59a1d37d5c81dcd2dbbb76") )
         ( 605960,   uint256("0x00000000000001b66d1b283d9da527e603a456bd3645ae35f0f4101c84c0de20") )
+        ( 644980,   uint256("0x00000000000000263fd4d52a9ad9fdcec5a047933595177781cdf7af937e3227") )
     ;
 
     // TestNet has no checkpoints
